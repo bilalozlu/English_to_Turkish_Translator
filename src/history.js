e433836db12e6e0ce3d030a1c898272bb2483fb2
bilalozlu <bilal5071@hotmail.com>
2022-08-24 06:38:33 +0300

    history toggle and scroll added

--- src/history.js
+++ src/history.js
@@ -1,13 +1,23 @@
-import './App.css';
+import { useState } from 'react';
+import './App.scss';
 
 function History(props) {
+    const [isHistoryOpen, setIsHistoryOpen] = useState(false);
     return (
         <div className="history">
-            {props.translatedList ? 
-                props.translatedList.map(a => 
-                <p key={a}>{a}</p>
-            )
-            : <p>You did not translate anything yet, try one please</p>}
+            <div className='historyToggle'>
+                <h2>History</h2>
+                <img src={"arrow.png"}
+                    className={`toggleImg ${isHistoryOpen ? 'open' : 'closed'}`}
+                    onClick={() => setIsHistoryOpen(!isHistoryOpen)} alt="history" />
+            </div>
+            {!isHistoryOpen ? null : 
+                props.translatedList ?
+                props.translatedList.map(a =>
+                    <p key={a}>{a}</p>
+                )
+                : <p>You did not translate anything yet, try one please</p>}
+
         </div>
     );
 }
