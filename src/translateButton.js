541c05b484f81d886b848125a2a0469bc216c536
bilalozlu <bilal5071@hotmail.com>
2022-08-27 07:06:49 +0300

    readding divs

--- src/translateButton.js
+++ src/translateButton.js
@@ -2,12 +2,12 @@ import './App.scss';
 
 function TranslateButton(props) {
     return (
-        <div>
+        <>
             <button className='translateButton' onClick={() => props.action()}>
                 <img src={"arrow.png"} className="arrowImg" alt="arrow" />
                 <p>Translate</p>
             </button>
-        </div>
+        </>
     );
 }
 
